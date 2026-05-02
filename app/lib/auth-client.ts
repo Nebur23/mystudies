797c16bb79e564bec415116a8748c9b6708fff4f
Nebur23 <yvesnyemb7@gmail.com>
2026-05-02 15:02:49 +0100

    fix baseUrl betterAuth v1

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,10 +1,8 @@
 import { createAuthClient } from "better-auth/react" // make sure to import from better-auth/react
 
-const isProduction = import.meta.env.MODE === "production"
-
 export const authClient = createAuthClient({
     //you can pass client configuration here
-    baseURL: isProduction ? window.location.origin : "http://localhost:5173"
+    baseURL: "https://mystudies-production.up.railway.app"
 })
 
 export const { useSession, getSession, signIn, signOut, signUp } = authClient;
