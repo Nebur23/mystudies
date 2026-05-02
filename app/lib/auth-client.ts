a1b0d81092eaed295b3917c0d7b25ba0e22aa6c4
Nebur23 <yvesnyemb7@gmail.com>
2026-05-02 14:58:05 +0100

    fix baseUrl betterAuth

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,8 +1,10 @@
 import { createAuthClient } from "better-auth/react" // make sure to import from better-auth/react
 
+const isProduction = import.meta.env.MODE === "production"
+
 export const authClient = createAuthClient({
     //you can pass client configuration here
-    baseURL: "http://localhost:5173"
+    baseURL: isProduction ? window.location.origin : "http://localhost:5173"
 })
 
 export const { useSession, getSession, signIn, signOut, signUp } = authClient;
