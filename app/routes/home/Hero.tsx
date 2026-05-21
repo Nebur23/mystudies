8401149e557b61176212a4a9326e42b5f1fb6c63
Nebur23 <yvesnyemb7@gmail.com>
2026-05-21 14:38:36 +0100

    fix/ home hero cta button link

--- app/routes/home/Hero.tsx
+++ app/routes/home/Hero.tsx
@@ -1,16 +1,16 @@
 import { Button } from "~/components/ui/button";
-import { buttonVariants } from "~/components/ui/button";
 import { HeroCards } from "./HeroCards";
-import { Link } from "react-router";
 import HeroImg from "~/assets/hero.png";
+import { useNavigate } from "react-router";
 
 export const Hero = () => {
+  const navigate = useNavigate()
   return (
     <section style={{ backgroundImage: `url(${HeroImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }} className=" container  grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-4 md:not-last:px-4">
       <div className="text-center lg:text-start space-y-6 px-4 ">
         <main className="text-5xl md:text-6xl font-bold ">
           <h1 className="inline font-sora">
-            <span className="inline bg-gradient-to-r from-[#1A56DB]  to-[#D247BF] text-transparent bg-clip-text">
+            <span className="inline bg-linear-to-r from-[#1A56DB]  to-[#D247BF] text-transparent bg-clip-text">
               Prepare for GCE <br />
             </span>
             the Right Way
@@ -23,15 +23,12 @@ export const Hero = () => {
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-         <Button size={"lg"} className="w-full md:w-1/3"> <Link to="/sign-in">Start Learning</Link> </Button>
+         <Button onClick={() => navigate("/practice")} size={"lg"} className="w-full md:w-1/3"> Start Learning</Button>
 
-        <Button size={"lg"} className="w-full md:w-1/3" variant={"outline"}>
-          <Link
-            rel="noreferrer noopener"
-            to="/courses"
-          >
+        <Button onClick={() => navigate("/courses")} size={"lg"} className="w-full md:w-1/3" variant={"outline"}>
+         
             Explore Courses
-          </Link>
+          
           </Button>
         </div>
       </div>
